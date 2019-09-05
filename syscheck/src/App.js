ffadc068b2ac435eb9a8360497ae015956e144e3
Carol Meng <cmeng@hit-play.com>
2019-09-05 13:54:27 -0400

    .

--- syscheck/src/App.js
+++ syscheck/src/App.js
@@ -1,8 +1,13 @@
 import React from "react";
 import "./App.css";
+import SysCheck from "./components/SysCheck";
+import DocCheck from "./components/DocCheck";
 
 function App() {
-  return <div className="App">hello</div>;
+  return <div className="App">
+    <SysCheck/>
+    <DocCheck/>
+  </div>;
 }
 
 export default App;
--- syscheck/src/components/DocCheck.js
+++ syscheck/src/components/DocCheck.js
@@ -1,13 +1,14 @@
-import React, { Component } from 'react'
+import React, { Component } from "react";
 
 export class DocCheck extends Component {
-    render() {
-        return (
-            <div>
-                123
-            </div>
-        )
-    }
+  render() {
+    return (
+      <div>
+        <p>Enter the </p>
+        <Button type="primary">Doc Check</Button>
+      </div>
+    );
+  }
 }
 
-export default DocCheck
+export default DocCheck;
