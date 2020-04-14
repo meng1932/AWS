af9cdb2ceb9a1327584ef8610edac3113d299075
Carol Meng <cmeng@hit-play.com>
2020-04-14 13:10:26 -0400

    2020-04-14 update sumary

--- uploadGoalTips/summary.js
+++ uploadGoalTips/summary.js
@@ -3,7 +3,7 @@ AWS.config.region = "us-east-1";
 const async = require("async");
 const geoTz = require("geo-tz");
 /****************change here *****************/
-const env = "staging";
+const env = "dev";
 /****************change above ****************/
 var ddb = new AWS.DynamoDB.DocumentClient();
 let clientTableName = `hpc-clients-${env}`;
@@ -18,11 +18,10 @@ var updateparams = item => {
     Key: {
       id: item.id
     },
-    UpdateExpression: "SET #TZ = :TZ, #AB = :AB",
-    ExpressionAttributeNames: { "#TZ": "goal", "#AB": "tips" },
+    UpdateExpression: "SET #TZ = :TZ",
+    ExpressionAttributeNames: { "#TZ": "summary" },
     ExpressionAttributeValues: {
-      ":TZ": 300,
-      ":AB": "This is the tips of the day"
+      ":TZ": "This is the summary"
     }
   };
 };
